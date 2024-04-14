74e6af9474c53cd0f5b11b38f6a2968efddec7c7
Aman Kumar <amank966180@gmail.com>
2024-04-14 18:44:35 +0530

    style fixed

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask,render_template,redirect,request
+from flask import Flask, render_template, redirect, request
 import rdflib
 app = Flask(__name__)
 g = rdflib.Graph()
 g.parse("KMSWT.rdf")
 
-@app.route('/subscriber',methods=['POST'])
+
+@app.route('/subscriber', methods=['POST'])
 def Subsriber():
     # First_Name = request.form.get('First_Name')
     # Last_Name = request.form.get('Last_Name')
@@ -49,24 +50,38 @@ def Subsriber():
     songs_names = []
     for r in g.query(q1):
         Endpoint_1 = r['title']
-        songs_names.append(Endpoint_1[ : ])
+        songs_names.append(Endpoint_1[:])
     artist_names = []
     for r in g.query(q2):
         Endpoint_2 = r['title']
-        artist_names.append(Endpoint_2[ : ])
-    return render_template('subscriber.html',songs_names=songs_names,artist_names=artist_names,Genre=Genre,Artist=Artist)
+        artist_names.append(Endpoint_2[:])
+    return render_template('subscriber.html', songs_names=songs_names, artist_names=artist_names, Genre=Genre, Artist=Artist)
+
 
 @app.route('/index')
 def hello_world():  # put application's code here
     return render_template('index.html')
 
+
+@app.route('/about')
+def about():  # put application's code here
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def contact():  # put application's code here
+    return render_template('contact.html')
+
+
 @app.route('/')
 def Main_Redirection():
     return redirect('/index')
 
+
 @app.route('/<string:url>')
 def Routes_Redirection(url):
     return redirect('/index')
+
 
 if __name__ == '__main__':
     app.run()
